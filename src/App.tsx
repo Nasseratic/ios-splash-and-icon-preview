a9469f821cc29e8e6bb53faabc6fd40139840b70
Mohamed Nasser <nasseratic@gmail.com>
2024-06-30 12:18:44 +0200

    use url query

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,19 @@ import styled from "styled-components";
 
 import { useRef, useState } from "react";
 import { Iphone } from "./containers";
-import { FaGithub, FaLinkedin } from "react-icons/fa";
-import { GiBrazilFlag } from "react-icons/gi";
-import { LiaFlagUsaSolid } from "react-icons/lia";
 import { useTranslation } from "react-i18next";
 
-export default function App() {
-  const { t, i18n } = useTranslation();
+const query = new URLSearchParams(window.location.search);
+const urlConfig = {
+  appName: query.get("appName") || "",
+  iconUrl: query.get("iconUrl") || "",
+};
 
-  const [appName, setAppName] = useState(t("yourAppName"));
-  const [selectedIcon, setSelectedIcon] = useState("");
+export default function App() {
+  const { t } = useTranslation();
+
+  const [appName, setAppName] = useState(urlConfig.appName);
+  const [selectedIcon, setSelectedIcon] = useState(urlConfig.iconUrl);
 
   const hiddenFileInput = useRef<HTMLInputElement>(null);
 
@@ -28,56 +31,25 @@ export default function App() {
     }
   }
 
-  function toggleLanguage() {
-    if (i18n.language === "pt-BR") {
-      i18n.changeLanguage("en");
-      document.title = "Preview Your Icon";
-      return;
-    }
-
-    document.title = "Prévia de Ícone";
-    i18n.changeLanguage("pt-BR");
-  }
-
   return (
     <Container>
-      <SocialsBar>
-        <span>{t("madeBy")} Saulo Joab </span>
-        <Link
-          onClick={() => open("https://github.com/saulojoab/preview-your-icon")}
-        >
-          <FaGithub size={18} />
-        </Link>
-        <Link onClick={() => open("https://linkedin.com/in/saulojoab")}>
-          <FaLinkedin size={18} />
-        </Link>
-        <Link onClick={toggleLanguage}>
-          {i18n.language === "pt-BR" ? (
-            <LiaFlagUsaSolid size={22} />
-          ) : (
-            <GiBrazilFlag size={18} />
-          )}
-        </Link>
-      </SocialsBar>
-      <TitleSubtitleContainer>
-        <Title>{t("previewYourIcon")}</Title>
-        <Subtitle>{t("wannaSeeHow")}</Subtitle>
-      </TitleSubtitleContainer>
-      <SelectAndInputContainer>
-        <SelectImageButton onClick={handleClickToUpload}>
-          {t("selectTheIcon")}
-        </SelectImageButton>
-        <HiddenInput
-          ref={hiddenFileInput}
-          type="file"
-          accept="image/*"
-          onChange={handleSelectImage}
-        />
-        <AppNameInput
-          placeholder={t("andTellMe")}
-          onChange={(e) => setAppName(e.target.value)}
-        />
-      </SelectAndInputContainer>
+      {!urlConfig.iconUrl && (
+        <SelectAndInputContainer>
+          <SelectImageButton onClick={handleClickToUpload}>
+            {t("selectTheIcon")}
+          </SelectImageButton>
+          <HiddenInput
+            ref={hiddenFileInput}
+            type="file"
+            accept="image/*"
+            onChange={handleSelectImage}
+          />
+          <AppNameInput
+            placeholder={t("andTellMe")}
+            onChange={(e) => setAppName(e.target.value)}
+          />
+        </SelectAndInputContainer>
+      )}
       <Iphone appName={appName} selectedIcon={selectedIcon} />
     </Container>
   );
@@ -90,37 +62,6 @@ const Container = styled.div`
   justify-content: center;
   width: 100%;
   min-height: 100vh;
-  background-size: 110% 110%;
-  background: ${({ theme }) =>
-    `linear-gradient(45deg, ${theme.colors.primary} 0%, ${theme.colors.black} 50%, ${theme.colors.primary} 100%)`};
-
-  font-family: ${(props) => props.theme.fonts.regular};
-  color: ${(props) => props.theme.colors.white};
-  transition: all 0.2s ease-in-out;
-
-  padding-top: 20px;
-  padding-bottom: 50px;
-`;
-
-const Title = styled.span`
-  font-size: 30px;
-  font-family: ${(props) => props.theme.fonts.bold};
-  margin-bottom: 5px;
-
-  ${({ theme }) => theme.breakpoints.down("sm")} {
-    font-size: 25px;
-  }
-`;
-
-const Subtitle = styled.span`
-  font-size: 18px;
-  font-family: ${(props) => props.theme.fonts.thin};
-  margin-bottom: 20px;
-  text-align: center;
-
-  ${({ theme }) => theme.breakpoints.down("sm")} {
-    font-size: 16px;
-  }
 `;
 
 const SelectAndInputContainer = styled.div`
@@ -181,34 +122,4 @@ const TitleSubtitleContainer = styled.div`
   justify-content: center;
   padding: 10px;
   padding-bottom: 0;
-`;
-
-const SocialsBar = styled.div`
-  position: fixed;
-  bottom: 10px;
-  right: 30px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: flex-start;
-  gap: 10px;
-  z-index: 10;
-  span {
-    font-family: ${({ theme }) => theme.fonts.thin};
-  }
-
-  ${({ theme }) => theme.breakpoints.down("sm")} {
-    position: relative;
-    align-self: center;
-    right: 0;
-    font-size: 13px;
-  }
-`;
-
-const Link = styled.a`
-  all: unset;
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  justify-content: center;
 `;
